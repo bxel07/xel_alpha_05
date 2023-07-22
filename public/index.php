8a40e7b28b0a41e82ffdb7c406fcdcd0f5ab82f0
bxel <yogipratama.ut@gmail.com>
2023-07-22 13:19:16 +0700

    adding helper

--- public/index.php
+++ public/index.php
@@ -1,12 +1,16 @@
 <?php
 $path = filter_var($_SERVER['PATH_INFO'] ?? "/", FILTER_SANITIZE_URL);
 if (str_starts_with($path, "/api/")) {
-    require_once __DIR__."/../router/api.php";
+    require_once __DIR__ . "/router/api.php";
     exit();
 } else {
-    require_once __DIR__."/../router/web.php";
+    require_once __DIR__ . "/router/web.php";
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+    $baseUrl = $protocol . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
+
+    $DATA = rtrim($baseUrl, '/') . '/' . ltrim($path, '/');
+    var_dump($DATA);
     exit();
 }
-
 
 
