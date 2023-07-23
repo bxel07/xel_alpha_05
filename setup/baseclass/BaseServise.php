41190be6a6c65e13b5e28194cb4646694017a0e4
bxel <yogipratama.ut@gmail.com>
2023-07-23 09:48:55 +0700

    adding display config to baseservice class

--- setup/baseclass/BaseServise.php
+++ setup/baseclass/BaseServise.php
@@ -3,16 +3,17 @@
 namespace setup\baseclass;
 use setup\config\BaseConn;
 use setup\config\xgen;
+use setup\config\Display;
 class BaseServise{
     protected $conn;
+    protected $render;
     public function __construct() {
         $conn = new BaseConn();
         $this->conn = $conn->getPDO();
     }
 
-    public function render():void {
-
-
+    public function render(string $path ='', array $data = [], string $name = '') {
+        return Display::render($path,$data,$name);
     }
 
     public function xgen(): xgen
